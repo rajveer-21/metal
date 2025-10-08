2fdcdf7de2ba8e1fab4e1b57f19131ed802a6e09
rajveer-21 <ajmanibhupa@gmail.com>
2025-10-08 20:06:31 +0530

    Delete RuntimeError.java

--- RuntimeError.java
+++
@@ -1,9 +0,0 @@
-class RuntimeError extends RuntimeException
-{
-    final Token token;
-    RuntimeError(Token token, String message)
-    {
-        super(message);
-        this.token = token;
-    }
-}
